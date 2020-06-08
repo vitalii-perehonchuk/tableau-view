2586b51bc8aa277c47c3ab8f7b0aa11c267e7874
undead404 <brute18@gmail.com>
2020-06-08 17:37:42 +0300

    other build

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,14 @@
 import childProcess from 'child_process';
+import fs from 'fs';
+import omit from 'lodash/omit';
+import package_ from '../package.json';
 
 childProcess.execSync('vue-cli-service build');
 childProcess.execSync(
-  'babel --config-file ./.server.babelrc src/server.js --out-file ./server.js',
+  'babel --config-file ./.server.babelrc src/server.js --out-file dist/server.js',
+);
+childProcess.execSync('cp Procfile dist/');
+fs.writeFileSync(
+  'dist/package.json',
+  JSON.stringify(omit(package_, ['devDependencies', 'scripts'])),
 );
